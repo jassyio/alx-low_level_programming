c6909a4af03d532078fe27ba6017c1df98d96803
jassyio <joejassyio@gmail.com>
2023-03-17 23:58:18 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,22 +7,22 @@
  */
 int main(void)
 {
-    char letters[52];
-    int i;
+char letters[52];
+int i;
 
-    /* Initialize the letters array with both lowercase and uppercase letters. */
-    for (i = 0; i < 26; i++)
-    {
-        letters[i] = 'a' + i;
-        letters[i + 26] = 'A' + i;
-    }
+/* Initialize the letters array with both lowercase and uppercase letters.*/
+for (i = 0; i < 26; i++)
+{
+letters[i] = 'a' + i;
+letters[i + 26] = 'A' + i;
+}
 
-    /* Print the letters array using putchar. */
-    for (i = 0; i < 52; i++)
-    {
-        putchar(letters[i]);
-    }
-    putchar('\n');
+/* Print the letters array using putchar.*/
+for (i = 0; i < 52; i++)
+{
+putchar(letters[i]);
+}
 
-    return (0);
+putchar('\n');
+return (0);
 }
