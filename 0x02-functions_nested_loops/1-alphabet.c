2e303445e19f093a5e6fc1ef1b9d28ac2e65b1e6
jassyio <joejassyio@gmail.com>
2023-03-21 11:26:42 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,22 +5,13 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+void print_alphabet(void) 
 {
-	char letters[26];
-	int i;
-
-	/* Initialize the letters array using a loop */
-	for (i = 0; i < 26; i++)
+	char c = 'a';
+	while (c <= 'z') 
 	{
-		letters[i] = 'a' + i;
-	}
-
-	/* Print the letters array using putchar */
-	for (i = 0; i < 26; i++)
-	{
-		_putchar(letters[i]);
+	_putchar(c);
+	c++;
 	}
 	_putchar('\n');
-	return (0);
 }
