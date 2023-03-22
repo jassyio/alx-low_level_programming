1998cf3da6b355a0f6ec9b701d5e9ca8f7b43c0d
jassyio <joejassyio@gmail.com>
2023-03-22 09:56:03 -0700

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n, first = 1, second = 2, next;
+    
+    printf("%d, %d", first, second);
+    
+    for (n = 3; n <= 50; n++)
+    {
+        next = first + second;
+        printf(", %d", next);
+        first = second;
+        second = next;
+    }
+    
+    printf("\n");
+    
+    return 0;
+}
+
