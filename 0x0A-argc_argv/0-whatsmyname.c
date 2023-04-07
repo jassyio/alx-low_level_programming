7fa32aafec456b1b4f3b4853c154540fb421c7e2
jassyio <j0ejassyio.com>
2023-04-06 23:10:40 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * main - prints the name of the file.
+ *argc - number or characters.
+ *argv - value of array
+ *@ return - always 0 (succes)
+ */
+
+int main(int argc, char **argv)
+	{
+	printf("%s\n", argv[0]);
+
+	return (0);
+	}
