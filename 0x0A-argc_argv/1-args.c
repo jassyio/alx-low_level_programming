cf406106ec31c46d0b83bdfb92d1c86ccd2b086f
jassyio <j0ejassyio.com>
2023-04-08 04:05:02 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main- prints the number of arguments
+ *@argc: the number of arguments
+ *@ argv: the value of arguments
+ * return - always - 0 (success)
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+	{
+		printf("%d\n", argc);
+
+		return (0);
+	}
