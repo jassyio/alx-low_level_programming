1b16abc734f3df4601f8017f204a9b48fde5468c
jassyio <j0ejassyio.com>
2023-04-10 10:22:38 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,10 @@
-#include<stdio.h>
-
+#include <stdio.h>
 /**
- * main- prints the name of the file.
- *@argc: number or characters.
- *@argv: value of array
- *return: always - 0 (succes)
+ * main - prints its name, followed by a new line.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
  */
-
 int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
